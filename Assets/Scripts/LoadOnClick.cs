5de038b4a069b567528e0ed5848ec4e8a5cda1cb
Sterling Miller <sparkstorm10@gmail.com>
2016-03-30 11:40:02 -0700

    This is all the changes for the new UI elements.

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public GameObject loadingImage;
+
+	public void LoadScene(int level)
+	{
+		//This loads the scene
+		loadingImage.SetActive (true);
+		Application.LoadLevel(level);
+
+	}
+}
