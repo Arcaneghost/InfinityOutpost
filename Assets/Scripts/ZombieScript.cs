a5d7a94f0253078709421c0f0dc0b01e2b7b0514
adajer <Jon>
2016-04-01 02:38:22 -0700

    yep

---
+++ Assets/Scripts/ZombieScript.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public class ZombieScript : MonoBehaviour {
+
+	//parameters
+	float distance;
+	RaycastHit hit;
+	float chaseSpeed = .2f;
+	float rotationSpeed = 2f;
+	float  attackThreshold = 1.5f;
+	float  chaseThreshold = 10f;
+	float  giveUpThreshold = 20f;
+	bool attacking = false;
+
+	///Waypoint Config
+	float accel = 0.8f;
+	float inertia = 0.9f;
+	float speedLimit = 2f;
+	float minSpeed = 1.0f;
+	float stopTime = 1.0f;
+
+	private float currentSpeed = 0.0f;
+	private bool accelState;
+	private bool slowState ;
+	private Transform waypoint;
+	float rotationDamping = 6.0f;
+	bool smoothRotation = true;
+	Transform [] waypoints;
+	private int WPindexPointer;
+
+
+	bool timeToChase = false;
+	bool timeToAttack = false;
+	float  attackRepeatTime = 1f;
+	public float  ZombieLife = 3f;
+	private float  functionState = 0f;
+	private bool chasing = false;
+	public Transform target;
+	public Transform myTransform;
+	public Animator anim;
+	public Rigidbody self;
+
+
+	void Awake(){
+		
+		anim = gameObject.gameObject.GetComponent<Animator> ();
+	}
+	void Start (){
+		functionState = 0;
+
+		}
+
+
+
+	void Update ()
+	{
+
+
+		distance = (target.position - myTransform.position).magnitude;
+
+		if (distance < 50f && distance > chaseThreshold)
+			anim.SetBool ("timeToChase", true);
+			timeToChase = true;
+
+		if (ZombieLife == 0) {
+			Destroy (gameObject);
+		}
+
+		//if (timeToChase) {
+		 
+
+			//myTransform.position += myTransform.forward * chaseSpeed * Time.deltaTime;
+			myTransform.rotation = Quaternion.Slerp (myTransform.rotation, Quaternion.LookRotation (target.position - myTransform.position), rotationSpeed * Time.deltaTime * rotationDamping);
+			
+
+			if (distance <= attackRepeatTime) {
+				anim.SetBool ("timeToAttack", true);
+				timeToAttack = true;
+				chaseSpeed = 0;
+				anim.SetBool ("timeToChase", false);
+				timeToChase = false;
+			}
+
+
+			if (distance >= chaseThreshold) {
+				anim.SetBool ("timeToChase", true);
+				timeToChase = true;
+				chaseSpeed = 5;
+				anim.SetBool ("timeToAttack", false);
+				timeToAttack = false;
+			}
+
+
+			if (distance > attackThreshold) {
+				chaseSpeed = 5;
+			}
+
+
+		//} else {
+		//if (distance == speedLimit) {
+		//		currentSpeed = speedLimit;
+		//	}
+		//}
+
+	}
+
+	void fixedUpdate (){
+		self.AddForce (0, 5, 0);
+		self.AddForce(target.position - myTransform.position);
+
+	}
+//	void OnTriggerEnter(Collider other) {
+//
+//		if(other.gameObject.CompareTag("Player"))
+//			{
+//				Debug.Log("You were hit");
+//			}
+//			if(other.gameObject.CompareTag("Waypoint")){
+//
+//
+//				functionState = 1;
+//				WPindexPointer++; ///when it collides with a waypoint, change waypoint to the next
+//
+//
+//				if(WPindexPointer >= waypoints.Length)
+//				{
+//					WPindexPointer = 0; //reset the active waypoint to the first
+//				}
+//
+//
+//			}
+//		}
+}
+
+		///if declared Slow();
+
+//		function Slow(){
+//			if(slowState == false){
+//				accelState = false;
+//				slowState = true;
+//			}
+//			///slow down (or speed ip if inertia is set above 1.0 in the inspector)
+//			currentSpeed = currentSpeed * inertia * Time.deltaTime;
+//			myTransform.Translate(0, 0, Time.deltaTime);
+//			if(currentSpeed <= minSpeed)
+//			{
+//				currentSpeed = 0.0;
+//				yield WaitForSeconds(stopTime); //wait for some time before moving to the next waypoint
+//				functionState = 0;
+//			}
+//		}
+//
---
+++ Assets/Scripts/ZombieupdatedScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieupdatedScript : MonoBehaviour {
+	
+
+	public float fpsTargetDistance;
+	public float enemyLookDistance;
+	public float attackDistance;
+	public float enemyMovementSpeed;
+	public float damping;
+	public Transform fpsTarget;
+	Rigidbody theRigidBody;
+
+	void Awake() {
+		
+			 }
+
+		// Use this for initialization
+	void Start () {
+		theRigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fpsTargetDistance = Vector3.Distance (fpsTarget.position, transform.position);
+		if (fpsTargetDistance < enemyLookDistance) 
+		{
+			lookAtPlayer ();
+		}
+
+		if (fpsTargetDistance < attackDistance)
+		{
+			attackPlease ();
+			print ("ATTACK");
+		}
+	}
+
+	void fixedUpdate (){
+		if (fpsTargetDistance < attackDistance) {
+			attackPlease ();
+			print ("ATTACK");
+		}
+	}
+	void lookAtPlayer()
+	{
+		Quaternion rotation = Quaternion.LookRotation (fpsTarget.position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime*damping);
+	}
+
+	void attackPlease()
+	{
+		theRigidBody.AddForce (transform.forward*enemyMovementSpeed);
+	}
+
+}
